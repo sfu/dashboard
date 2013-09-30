62594eb8c2abda3deb28e012d6c13e465ce8ee22
Graham Ballantyne <grahamb@sfu.ca>
2013-09-30 11:42:23 -0700

    create github job

---
+++ jobs/github.rb
@@ -0,0 +1,31 @@
+require 'github_api'
+
+def get_status_label(id, value)
+  levels = ['safe', 'warning', 'danger']
+  thresholds = {
+    "github_pull_requests" => [0, 1, 3],
+    "github_branch_comparison" => [0, 3, 5]
+  }
+
+  level = levels[0]
+
+  if thresholds[id].index value
+    level = levels[thresholds[id].index value]
+  else
+    if value >= thresholds[id][2]
+      level = levels[2]
+    elsif value >= thresholds[id][1]
+      level = levels[1]
+    end
+  end
+  level
+end
+
+
+SCHEDULER.every '1m', :first_in => '20s' do
+  pulls = github.pulls.all 'sfu', 'canvas-lms'
+  compare = github.repos.commits.compare 'sfu', 'canvas-lms', 'sfu-deploy', 'sfu-develop'
+
+  send_event('github_pull_requests', { current: pulls.count, status: get_status_label('github_pull_requests', pulls.count) })
+  send_event('github_branch_comparison', { current: compare.ahead_by, status: get_status_label('github_branch_comparison', compare.ahead_by) })
+end
