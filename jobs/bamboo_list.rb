4a9666a0fee4e1ea977871b2b80bfbfb749f5c5a
Graham Ballantyne <grahamb@sfu.ca>
2013-09-30 11:41:11 -0700

    use settings object for config settings

--- jobs/bamboo_list.rb
+++ jobs/bamboo_list.rb
@@ -1,27 +1,27 @@
 require 'bamboo_api'
-require 'yaml'
-config = YAML.load(File.read('./config/bamboo.yml')) rescue "No config/bamboo.yml file found."
 
-BambooApi.new({
-  end_point: config[:bamboo_host],
-  username: config[:bamboo_username],
-  password: config[:bamboo_password]
-})
+SCHEDULER.every '30s' do
+  BambooApi.new({
+    end_point: settings.bamboo[:bamboo_host],
+    username: settings.bamboo[:bamboo_username],
+    password: settings.bamboo[:bamboo_password]
+  })
 
-def get_plan_status(plan)
-  build = BambooApi::Build.find_by_plan(plan).first
-  data = {
-    name: build.plan_name,
-    build_number: build.number,
-    key: build.key,
-    username: build.username,
-    commit_id: build.vcs_revision_key,
-    status: build.state,
-    build_relative_time: build.build_relative_time
-  }
-end
-
-SCHEDULER.every '30s', :first_in => 0 do
-  items = config[:plan_keys].map { |p| get_plan_status(p) }
+  def get_plan_status(plan)
+    build = BambooApi::Build.find_by_plan(plan).first
+    data = {
+      name: build.plan_name,
+      build_number: build.number,
+      key: build.key,
+      username: build.username,
+      commit_id: build.vcs_revision_key,
+      status: build.state.downcase,
+      build_relative_time: build.build_relative_time,
+      icon: "icon-#{build.state.downcase}"
+    }
+    data[:name].slice! 'Deploy to '
+    data
+  end
+  items = settings.bamboo[:plan_keys].map { |p| get_plan_status(p) }
   send_event('bamboo_list', { items: items })
 end
