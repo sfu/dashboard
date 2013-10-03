2e870ecc5460665e1a88f640885468459b25074b
Graham Ballantyne <grahamb@sfu.ca>
2013-10-03 15:31:39 -0700

    rewrite google_analytics job to fetch data from caching server

--- jobs/google_analytics.rb
+++ jobs/google_analytics.rb
@@ -1,42 +1,25 @@
-require 'google/api_client'
-require 'date'
+require 'net/http'
+require 'uri'
+require 'json'
 
 current_count = 0
 
-SCHEDULER.every '60s' do
+def get_stats(url)
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true if uri.scheme === 'https'
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Get.new(uri.request_uri)
+
+  response = http.request(request)
+  JSON.parse(response.body)
+end
+
+
+SCHEDULER.every '30s' do
   last_count = current_count
-
-  client = Google::APIClient.new({
-    :application_name => 'Canvas Dashboard',
-    :application_version => '1.0'
-  })
-
-  # Load our credentials for the service account
-  key = Google::APIClient::KeyUtils.load_from_pkcs12("#{settings.root}/#{settings.google_analytics[:key_file]}", 'notasecret')
-  client.authorization = Signet::OAuth2::Client.new(
-    :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-    :audience => 'https://accounts.google.com/o/oauth2/token',
-    :scope => 'https://www.googleapis.com/auth/analytics.readonly',
-    :issuer => settings.google_analytics[:service_account_email],
-    :signing_key => key)
-
-  # Request a token for our service account
-  client.authorization.fetch_access_token!
-
-  analytics = client.discovered_api('analytics','v3')
-
-  startDate = DateTime.now.prev_day.strftime("%Y-%m-%d")
-  endDate = DateTime.now.strftime("%Y-%m-%d")
-
-  results = client.execute(:api_method => analytics.data.realtime.get, :parameters => {
-    'ids' => settings.google_analytics[:profile_id],
-    'start-date' => startDate,
-    'end-date' => endDate,
-    'metrics' => "ga:activeVisitors",
-    'fields' => "totalsForAllResults"
-  })
-
-  current_count = results.data.totals_for_all_results['ga:activeVisitors']
+  stats = get_stats(settings.google_analytics[:cache_url])
+  current_count = stats['data']['ga:activevisitors']
   send_event('active_users', { current: current_count, last: last_count })
-
 end
