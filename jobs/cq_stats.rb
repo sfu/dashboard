eb457bc0e6541fc48c5ad399e755d956829e5172
Jordan Raine <jnraine@gmail.com>
2013-11-22 16:09:43 -0800

    Add CQ stats and rearrange things a bit

---
+++ jobs/cq_stats.rb
@@ -0,0 +1,68 @@
+require 'rest-client'
+require 'json'
+
+def published_pages(cq_node)
+  response = RestClient.get url(cq_node).to_s, params: {
+    type: "cq:Page",
+    path: "/content/sfu"
+  }
+  data = JSON.parse(response)
+  data.fetch("total").to_i.humanize
+end
+
+def published_assets(cq_node)
+  response = RestClient.get url(cq_node).to_s, params: {
+    type: "dam:Asset",
+    path: "/content/dam/sfu"
+  }
+  data = JSON.parse(response)
+  data.fetch("total").to_i.humanize
+end
+
+def top_level_sites(cq_node)
+  response = RestClient.get url(cq_node).to_s, params: {
+    type: "cq:Page",
+    path: "/content",
+    nodename: "sfu",
+    "p.limit" => "1",
+    "p.hits" => "full",
+    "p.nodedepth" => 1
+  }
+  data = JSON.parse(response)
+
+  # Mungle the data to find out how many children
+  sfu_node_data = data.fetch("hits").first
+  sfu_node_data.delete_if {|k, v| !v.is_a?(Hash) }
+  sfu_node_data.keys.count.humanize
+end
+
+def url(cq_node)
+  url = URI.parse cq_node.fetch(:hostname)
+  url.user = cq_node.fetch(:username)
+  url.password = cq_node.fetch(:password)
+  url.path = "/bin/querybuilder.json"
+  url
+end
+
+class Fixnum
+  def humanize
+    self.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse
+  end
+end
+
+SCHEDULER.every '10s', first_in: '1s' do
+  cq_node = settings.cq.fetch(:publisher_p4)
+
+  stats = {
+    "Top-level Sites" => top_level_sites(cq_node),
+    "Published Pages" => published_pages(cq_node),
+    "Published Assets" => published_assets(cq_node)
+  }
+
+  data = []
+  stats.each do |label, value|
+    data << { label: label, value: value }
+  end
+
+  send_event('cq_stats', {items: data})
+end
